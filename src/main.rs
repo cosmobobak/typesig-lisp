fd003464c84ecfde7a826c2c84b393f82e946f20
cosmobobak <cosmobobak@gmail.com>
2024-09-27 21:13:58 +0100

    part 1

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+#![warn(clippy::all, clippy::nursery, clippy::pedantic)]
+
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Write},
+    path::PathBuf, time::Instant,
+};
+
+use clap::Parser;
+use color_eyre::eyre::Context;
+use color_eyre::Report;
+use color_eyre::Result;
+
+#[derive(Parser)]
+struct Args {
+    /// An input file. Replaces stdin.
+    #[clap(long, short)]
+    file: Option<PathBuf>,
+}
+
+struct InterpreterState {
+    show_timings: bool,
+}
+
+#[allow(clippy::derivable_impls)]
+impl Default for InterpreterState {
+    fn default() -> Self {
+        Self {
+            show_timings: false,
+        }
+    }
+}
+
+trait Mode {
+    const INTERACTIVE: bool;
+}
+
+struct Interactive;
+struct NonInteractive;
+
+impl Mode for Interactive {
+    const INTERACTIVE: bool = true;
+}
+impl Mode for NonInteractive {
+    const INTERACTIVE: bool = false;
+}
+
+fn repl<M, I, E>(input: I) -> Result<()>
+where
+    M: Mode,
+    I: Iterator<Item = Result<String, E>>,
+    Report: From<E>,
+{
+    let mut state = InterpreterState::default();
+    if M::INTERACTIVE {
+        println!("Welcome to Cosmo's LISP interpreter! Enter \"quit\", \"exit\", or EOF to exit.");
+        print!("lisp> ");
+        std::io::stdout().flush()?;
+    }
+    for line in input {
+        let start = Instant::now();
+        let line = line?;
+        match line.trim() {
+            "" => (),
+            "quit" | "exit" => return Ok(()),
+            ":timing" => state.show_timings = true,
+            line => println!("{line}"),
+        }
+
+        if M::INTERACTIVE {
+            if state.show_timings {
+                println!("completed in {}ns", start.elapsed().as_nanos());
+            }
+            print!("lisp> ");
+            std::io::stdout().flush()?;
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let args = Args::parse();
+    if let Some(path) = args.file {
+        let file = File::open(&path)
+            .with_context(|| format!("failed to open file at {}", path.display()))?;
+        let reader = BufReader::new(file);
+        repl::<NonInteractive, _, _>(reader.lines())?;
+    } else {
+        repl::<Interactive, _, _>(std::io::stdin().lines())?;
+    };
+
+    Ok(())
+}
